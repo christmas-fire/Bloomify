3f3bd69176419dfc524025905bef7f6f64790bf0
christmas-fire <vpiskin335@gmail.com>
2024-12-23 23:32:00 +0700

    Реализую эндпоинты для сущности Flower

---
+++ internal/api/flower.go
@@ -0,0 +1,94 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/christmas-fire/Bloomify/internal/models"
+	"github.com/christmas-fire/Bloomify/internal/repository/postgres.go"
+)
+
+type FlowerHandler struct {
+	repo postgres.FlowerRepository
+}
+
+func NewFlowerHandler(repo postgres.FlowerRepository) *FlowerHandler {
+	return &FlowerHandler{repo: repo}
+}
+
+func (h *FlowerHandler) AddFlower() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var f models.Flower
+
+		if err := json.NewDecoder(r.Body).Decode(&f); err != nil {
+			http.Error(w, "error decode json", http.StatusBadRequest)
+			return
+		}
+
+		if err := h.repo.AddFlower(f); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(f)
+
+		log.Printf("flower '%s' has added", f.Name)
+	}
+}
+
+func (h *FlowerHandler) GetAllFlowers() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		flowers, err := h.repo.GetAllFlowers()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(flowers)
+	}
+}
+
+func (h *FlowerHandler) GetFlowerByID() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		path := strings.TrimPrefix(r.URL.Path, "/flowers/")
+		if path == "" {
+			http.Error(w, "flower ID is required", http.StatusBadRequest)
+			return
+		}
+
+		id, err := strconv.Atoi(path)
+		if err != nil {
+			http.Error(w, "invalid user ID", http.StatusBadRequest)
+			return
+		}
+
+		flower, err := h.repo.GetFlowerByID(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(flower)
+	}
+}
