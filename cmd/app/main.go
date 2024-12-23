497b0ef8e780532366429f9df1aba6b09feba549
christmas-fire <vpiskin335@gmail.com>
2024-12-23 23:33:01 +0700

    Добавил маршруты для эндпоинтов /flowers

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -18,12 +18,19 @@ func main() {
 	userRepo := postgres.NewUserRepository(db)
 	userHandler := api.NewUserHandler(*userRepo)
 
+	flowerRepo := postgres.NewFlowerRepository(db)
+	flowerHandler := api.NewFlowerHandler(*flowerRepo)
+
 	r := http.DefaultServeMux
 
-	r.HandleFunc("/register", userHandler.SignUp())
-	r.HandleFunc("/login", userHandler.SignIn())
-	r.HandleFunc("/users", userHandler.GetAllTasks())
-	r.HandleFunc("/delete", userHandler.DeleteUser())
+	r.Handle("/users/register", userHandler.SignUp())       // Регистрация
+	r.HandleFunc("/users/login", userHandler.SignIn())      // Логин
+	r.HandleFunc("/users", userHandler.GetAllUsers())       // Получение всех пользователей
+	r.HandleFunc("/users/delete", userHandler.DeleteUser()) // Удаление пользователя
+
+	r.HandleFunc("/flowers/add", flowerHandler.AddFlower())      // Добавление нового цветка
+	r.HandleFunc("/flowers", flowerHandler.GetAllFlowers())      // Получение всех цветов
+	r.HandleFunc("/flowers/{id}", flowerHandler.GetFlowerByID()) // Получение цветка по ID
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
