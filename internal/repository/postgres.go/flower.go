3afc8b1a3d1993c65fb1c2144cbc418aa940ebc2
christmas-fire <vpiskin335@gmail.com>
2024-12-23 23:29:47 +0700

    Реализую CRUD операции с сущностью Flower

--- internal/repository/postgres.go/flower.go
+++ internal/repository/postgres.go/flower.go
@@ -1,1 +1,108 @@
 package postgres
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/christmas-fire/Bloomify/internal/models"
+)
+
+type FlowerRepository struct {
+	db *sql.DB
+}
+
+func NewFlowerRepository(db *sql.DB) *FlowerRepository {
+	return &FlowerRepository{db: db}
+}
+
+func ValidateFlower(f models.Flower) error {
+	if f.Name == "" {
+		return fmt.Errorf("name can't be empty")
+	}
+	if f.Price <= 0 {
+		return fmt.Errorf("price can't be less or equal 0")
+	}
+	if f.Stock < 0 {
+		return fmt.Errorf("stock can't negative number")
+	}
+	return nil
+}
+
+func (r *FlowerRepository) AddFlower(f models.Flower) error {
+	checkQuery := `
+		SELECT EXISTS (
+			SELECT 1
+			FROM flowers
+			WHERE name = $1
+	)`
+
+	insertQuery := `
+		INSERT INTO flowers (name, price, stock)
+		VALUES ($1, $2, $3)
+	`
+
+	if err := ValidateFlower(f); err != nil {
+		return fmt.Errorf("invalid flower data: %w", err)
+	}
+
+	var exists bool
+	if err := r.db.QueryRow(checkQuery, f.Name).Scan(&exists); err != nil {
+		return fmt.Errorf("error check if flower exists: %w", err)
+	}
+
+	if exists {
+		return fmt.Errorf("flower already exists")
+	}
+
+	if _, err := r.db.Exec(insertQuery, f.Name, f.Price, f.Stock); err != nil {
+		return fmt.Errorf("error inserting new flower into database: %w", err)
+	}
+
+	return nil
+}
+
+func (r *FlowerRepository) GetAllFlowers() ([]models.Flower, error) {
+	query := `
+		SELECT id, name, price, stock FROM flowers
+	`
+
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var flowers []models.Flower
+	for rows.Next() {
+		var f models.Flower
+		if err := rows.Scan(&f.Id, &f.Name, &f.Price, &f.Stock); err != nil {
+			return nil, err
+		}
+		flowers = append(flowers, f)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return flowers, nil
+}
+
+func (r *FlowerRepository) GetFlowerByID(id int) (*models.Flower, error) {
+	query := `
+		SELECT id, name, price, stock
+		FROM flowers
+		WHERE id = $1
+	`
+
+	var f models.Flower
+	err := r.db.QueryRow(query, id).Scan(&f.Id, &f.Name, &f.Price, &f.Stock)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("flower with id %d not found", id)
+		}
+		return nil, fmt.Errorf("error querying flower by id: %w", err)
+	}
+
+	return &f, nil
+}
