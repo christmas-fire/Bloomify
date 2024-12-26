d5be7f971ec862948728897d71ddf488f992bfb7
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2024-12-26 17:18:30 +0700

    Теперь все иннициализации происходят в пакете app. В main.go лишь иннициализиуется экземпляр App и вызываются его методы

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,41 +1,18 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/christmas-fire/Bloomify/internal/api"
-	"github.com/christmas-fire/Bloomify/internal/database"
-	"github.com/christmas-fire/Bloomify/internal/repository/postgres.go"
+	"github.com/christmas-fire/Bloomify/internal/app"
 )
 
 func main() {
-	db := database.InitPostgres()
-	if err := database.InitTables(db); err != nil {
+	app, err := app.NewApp()
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	client := database.InitRedis(context.Background())
-	fmt.Println(client)
-
-	userRepo := postgres.NewUserRepository(db)
-	userHandler := api.NewUserHandler(*userRepo)
-
-	flowerRepo := postgres.NewFlowerRepository(db)
-	flowerHandler := api.NewFlowerHandler(*flowerRepo)
-
-	r := http.DefaultServeMux
-
-	r.Handle("/users/register", userHandler.SignUp())       // Регистрация
-	r.HandleFunc("/users/login", userHandler.SignIn())      // Логин
-	r.HandleFunc("/users", userHandler.GetAllUsers())       // Получение всех пользователей
-	r.HandleFunc("/users/delete", userHandler.DeleteUser()) // Удаление пользователя
-
-	r.HandleFunc("/flowers/add", flowerHandler.AddFlower())      // Добавление нового цветка
-	r.HandleFunc("/flowers", flowerHandler.GetAllFlowers())      // Получение всех цветов
-	r.HandleFunc("/flowers/{id}", flowerHandler.GetFlowerByID()) // Получение цветка по ID
-
-	log.Fatal(http.ListenAndServe(":8080", r))
+	if err := app.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
