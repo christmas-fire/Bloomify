fe87966fef325448afc67e0786f95898f417d2cb
christmas-fire <vpiskin335@gmail.com>
2025-01-19 00:48:51 +0700

    Написал тесты для обработчиков auth

--- internal/controller/auth_test.go
+++ internal/controller/auth_test.go
@@ -94,3 +94,78 @@ func TestHandler_signUp(t *testing.T) {
 		})
 	}
 }
+
+func TestHandler_signIn(t *testing.T) {
+	// Init Test Table
+	type mockBehavior func(r *mock_service.MockAuth, input signInInput)
+	tests := []struct {
+		name                 string
+		inputBody            string
+		inputSignIn          signInInput
+		mockBehavior         mockBehavior
+		expectedStatusCode   int
+		expectedResponseBody string
+	}{
+		{
+			name:      "Ok",
+			inputBody: `{"username": "username", "password": "qwerty"}`,
+			inputSignIn: signInInput{
+				Username: "username",
+				Password: "qwerty",
+			},
+			mockBehavior: func(r *mock_service.MockAuth, input signInInput) {
+				r.EXPECT().GenerateToken(input.Username, input.Password).Return("token", nil)
+			},
+			expectedStatusCode:   201,
+			expectedResponseBody: `{"token":"token"}`,
+		},
+		{
+			name:                 "Wrong Input",
+			inputBody:            `{"username": "username"}`,
+			inputSignIn:          signInInput{},
+			mockBehavior:         func(r *mock_service.MockAuth, input signInInput) {},
+			expectedStatusCode:   400,
+			expectedResponseBody: `{"message":"Key: 'signInInput.Password' Error:Field validation for 'Password' failed on the 'required' tag"}`,
+		},
+		{
+			name:      "Service Error",
+			inputBody: `{"username": "username", "password": "qwerty"}`,
+			inputSignIn: signInInput{
+				Username: "username",
+				Password: "qwerty",
+			},
+			mockBehavior: func(r *mock_service.MockAuth, input signInInput) {
+				r.EXPECT().GenerateToken(input.Username, input.Password).Return("", errors.New("something went wrong"))
+			},
+			expectedStatusCode:   500,
+			expectedResponseBody: `{"message":"something went wrong"}`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Init Dependencies
+			c := gomock.NewController(t)
+			defer c.Finish()
+			repo := mock_service.NewMockAuth(c)
+			test.mockBehavior(repo, test.inputSignIn)
+			services := &service.Service{Auth: repo}
+			handler := Handler{services}
+
+			// Init Endpoint
+			r := gin.New()
+			r.POST("/auth/sign-in", handler.signIn)
+
+			// Create Request
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/auth/sign-in",
+				bytes.NewBufferString(test.inputBody))
+
+			// Make Request
+			r.ServeHTTP(w, req)
+
+			// Assert
+			assert.Equal(t, w.Code, test.expectedStatusCode)
+			assert.Equal(t, w.Body.String(), test.expectedResponseBody)
+		})
+	}
+}
